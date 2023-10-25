e13a03fc6fdefbb97076c4ef25f9e04dcb61567a
Francesco <francesco.eletto02@gmail.com>
2023-10-25 17:40:09 +0200

    ASD2023-24/L03/E01/main.c implemented vector allocation and reading in main function

--- ASD2023-24/L03/E01/main.c
+++ ASD2023-24/L03/E01/main.c
@@ -1,6 +1,17 @@
 #include "stdio.h"
+#include "malloc.h"
 
 int main(){
-    printf("welcome");
+    int N, *vec;
+
+    printf("Dimensione del vettore:\n");
+    scanf("%d", &N);
+    vec = (int *)malloc(N*sizeof(int));
+    printf("Digita i %d interi: ", N);
+    for (int i = 0; i < N; ++i) {
+        scanf("%d",&vec[i]);
+
+    }
+
     return 0;
 }
