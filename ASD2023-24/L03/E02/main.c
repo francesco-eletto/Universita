d6e6a37de7bf0fe83c87baf582726b5a9e78c7dc
Francesco <francesco.eletto02@gmail.com>
2023-10-26 17:39:30 +0200

    ASD2023-24/L03/E01/main.c implemented multiplyAlgorithm

--- ASD2023-24/L03/E02/main.c
+++ ASD2023-24/L03/E02/main.c
@@ -4,19 +4,48 @@
 #define MAXLEN 255
 
 typedef struct {
-    char brani[5][255];
+    char brani[5][MAXLEN];
     int num;
 }s_playlistPerUser;
 
 int fileRead(char *fileName, s_playlistPerUser **playlistPerUser);
+int multiplyAlgorithm(s_playlistPerUser *playlistPerUser, int len);
+int r_multiplyAlgorithm(char **sol, int pos, int n, s_playlistPerUser *playlistPerUser, int count);
 
 int main() {
     s_playlistPerUser *playlistPerUser;
     int len;
 
     len = fileRead("../brani.txt", &playlistPerUser);
+    multiplyAlgorithm(playlistPerUser,len);
 
     return 0;
+}
+
+int multiplyAlgorithm(s_playlistPerUser *playlistPerUser, int len){
+    char **sol;
+
+    sol = (char **) malloc(len*sizeof(char *));
+    for (int i = 0; i < len; ++i) {
+        sol[i] = (char *) malloc(MAXLEN * sizeof(char));
+    }
+    return r_multiplyAlgorithm(sol,0,len,playlistPerUser,0);
+}
+
+int r_multiplyAlgorithm(char **sol, int pos, int n, s_playlistPerUser *playlistPerUser, int count){
+    if(pos >= n){
+        for (int i = 0; i < n; ++i) {
+            printf("%s ", sol[i]);
+        }
+        printf("\n");
+        return count + 1;
+    }
+
+    for (int i = 0; i < playlistPerUser[pos].num; ++i) {
+        strcpy(sol[pos], playlistPerUser[pos].brani[i]);
+        r_multiplyAlgorithm(sol,pos+1,n,playlistPerUser,count);
+    }
+    return count;
 }
 
 int fileRead(char *fileName, s_playlistPerUser **playlistPerUser){
