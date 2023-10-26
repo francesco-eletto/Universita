6415f1c32eb36df0722c21a7966d91994820af48
Francesco <francesco.eletto02@gmail.com>
2023-10-26 16:28:47 +0200

    added fileRead function

--- ASD2023-24/L03/E02/main.c
+++ ASD2023-24/L03/E02/main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include "malloc.h"
+#define MAXLEN 255
+
+typedef struct {
+    char brani[5][255];
+    int num;
+}s_playlistPerUser;
+
+int fileRead(char *fileName, s_playlistPerUser **playlistPerUser);
 
 int main() {
-    printf("Hello, World!\n");
+    s_playlistPerUser *playlistPerUser;
+    int len;
+
+    len = fileRead("../brani.txt", &playlistPerUser);
+
     return 0;
+}
+
+int fileRead(char *fileName, s_playlistPerUser **playlistPerUser){
+    FILE *fIn;
+    int len;
+    s_playlistPerUser *tmp_playlistPerUser;
+
+    fIn =fopen(fileName, "r");
+    fscanf(fIn,"%d", &len);
+    tmp_playlistPerUser = (s_playlistPerUser *)malloc(len* sizeof(s_playlistPerUser));
+    playlistPerUser = tmp_playlistPerUser;
+    return len;
+
 }
