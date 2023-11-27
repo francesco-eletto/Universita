e3ea9cbe6536daf77b9ea00b44e8c326e9b1df8d
Francesco <francesco.eletto02@gmail.com>
2023-11-27 18:35:42 +0100

    ASD2023-24/L04/E01/main.c fixed struct and pointer naming

--- ASD2023-24/L04/E01/main.c
+++ ASD2023-24/L04/E01/main.c
@@ -1,68 +1,68 @@
 #include <stdio.h>
 #include <malloc.h>
 
-typedef struct grafo {
+typedef struct graph {
     int N, E;
     int **archi;
-}*sp_grafo;
+}s_graph;
 
-void graphRead(struct grafo *sp_grafo, char *fileName);
-struct grafo *graphInit();
+void graphRead(s_graph *p_graph, char *fileName);
+s_graph *graphInit();
 void partsAlgorithm();
 void partsAlgorithmR();
-int check(int *val, int *sol, struct grafo *p_grafo);
+int check(int *val, int *sol, s_graph *p_graph);
 
 int main(int argc, char **argv) {
 
-    struct grafo *p_grafo;
+    s_graph *p_graph;
 
-    p_grafo = graphInit();
-    graphRead(p_grafo, argv[1]);
-    partsAlgorithm(p_grafo->N, p_grafo);
+    p_graph = graphInit();
+    graphRead(p_graph, argv[1]);
+    partsAlgorithm(p_graph->N, p_graph);
 
     return 0;
 }
 
-struct grafo *graphInit(){
-    struct grafo *p_grafoTmp;
+s_graph *graphInit(){
+    s_graph *p_graphTmp;
 
-    p_grafoTmp = malloc(sizeof(*p_grafoTmp));
+    p_graphTmp = malloc(sizeof(*p_graphTmp));
 
-    return p_grafoTmp;
+    return p_graphTmp;
 }
 
-void graphRead(struct grafo *sp_grafo, char *fileName){
+void graphRead(s_graph *p_graph, char *fileName){
 
     FILE *fIn;
 
     fIn = fopen(fileName, "r");
-    fscanf(fIn,"%d", &(sp_grafo->N));
-    fscanf(fIn,"%d", &(sp_grafo->E));
+    fscanf(fIn,"%d", &(p_graph->N));
+    fscanf(fIn,"%d", &(p_graph->E));
 
-    sp_grafo->archi = (int **)malloc(sp_grafo->E * sizeof(int *));
-    for (int i = 0; i < sp_grafo->E; ++i) {
-        sp_grafo->archi[i] = (int *) malloc(2*sizeof(int));
+    p_graph->archi = (int **)malloc(p_graph->E * sizeof(int *));
+    for (int i = 0; i < p_graph->E; ++i) {
+        p_graph->archi[i] = (int *) malloc(2 * sizeof(int));
     }
 
-    for (int i = 0; i < sp_grafo->E; ++i) {
+    for (int i = 0; i < p_graph->E; ++i) {
         for (int j = 0; j < 2; ++j) {
-            fscanf(fIn, "%d", &(sp_grafo->archi[i][j]));
+            fscanf(fIn, "%d", &(p_graph->archi[i][j]));
         }
     }
 }
 
-int check(int *val, int *sol, struct grafo *p_grafo){
+int check(int *val, int *sol, s_graph *p_graph){
 
     int success = 1;
 
-    for (int i = 0; i < p_grafo->E; ++i) {
-        if(!(sol[p_grafo->archi[i][0]] || sol[p_grafo->archi[i][1]])) success = 0;
+    for (int i = 0; i < p_graph->E; ++i) {
+        if(!(sol[p_graph->archi[i][0]] || sol[p_graph->archi[i][1]])) success = 0;
     }
     return success;
 
 }
 
-void partsAlgorithm(int n, struct grafo *p_grafo){
+void partsAlgorithm(int n, s_graph *p_graph){
 
     int *val, *sol;
 
@@ -71,13 +71,13 @@ void partsAlgorithm(int n, struct grafo *p_grafo){
     for (int i = 0; i < n; ++i) {
         val[i] = i;
     }
-    partsAlgorithmR(0,val,sol,n,p_grafo);
+    partsAlgorithmR(0, val, sol, n, p_graph);
 
 }
 
-void partsAlgorithmR(int pos, int *val, int *sol, int n, struct grafo *p_grafo){
+void partsAlgorithmR(int pos, int *val, int *sol, int n, s_graph *p_graph){
     if(pos >= n){
-        if(check(val, sol, p_grafo)){
+        if(check(val, sol, p_graph)){
             for (int i = 0; i < n; ++i) {
                 if(sol[i]!=0) {
                     printf("%d ", val[i]);
@@ -89,9 +89,9 @@ void partsAlgorithmR(int pos, int *val, int *sol, int n, struct grafo *p_grafo){
     }
 
     sol[pos] = 0;
-    partsAlgorithmR(pos+1,val,sol,n,p_grafo);
+    partsAlgorithmR(pos+1, val, sol, n, p_graph);
     sol[pos] = 1;
-    partsAlgorithmR(pos+1,val,sol,n,p_grafo);
+    partsAlgorithmR(pos+1, val, sol, n, p_graph);
     return;
 
 }
