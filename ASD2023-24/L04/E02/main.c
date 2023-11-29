1e8cb4d28718ee5d1c6172fac70232a111f94aad
Francesco <francesco.eletto02@gmail.com>
2023-11-29 18:36:59 +0100

    ASD2023/24/L04/E02/main.c menù added. deleteByCode and deleteByDataRange bug fixed(if head == NULL). deleteByCode now returns item now

--- ASD2023-24/L04/E02/main.c
+++ ASD2023-24/L04/E02/main.c
@@ -3,6 +3,7 @@
 #include <malloc.h>
 #include <string.h>
 #define MAXLEN 50
+#define OUTFILENAME "../output.txt"
 
 typedef struct{
     int giorno, mese, anno;
@@ -31,15 +32,17 @@ typedef struct{
 
 int found = 1;
 
+
+
 s_list *newList();
 s_node *newNode(s_node *next, s_item);
 int dataGrater(s_data data1, s_data data2);
 s_node *listSortInsert(s_list *p_list, s_item item);
-void fileRead(char *fileName, s_list *p_list);
+int fileRead(char *fileName, s_list *p_list);
 s_item searchByCode(int code, s_list *p_list);
 s_item itemVoid();
 void itemPrint(s_item item);
-s_node *deleteByCode(int code, s_list *p_list);
+s_item deleteByCode(int code, s_list *p_list);
 s_item deleteByDataRange(s_data data1, s_data data2, s_list *p_list);
 void printOnFile(char *file, s_list *p_list);
 
@@ -48,32 +51,92 @@ int main() {
 
     s_list *p_list;
     p_list = newList();
-
-    /*fileRead("../anag1.txt",p_list);
-    fileRead("../anag2.txt",p_list);
-
-
-    s_data data1, data2;
-
-    data1.anno = 1994;
-    data1.mese = 11;
-    data1.giorno = 2;
-    data2.anno = 2002;
-    data2.mese = 9;
-    data2.giorno = 26;
-
     s_item TMP_item;
+    s_data data1, data2;
+    int cmdIndex, TMP_code, readReturn;
+    char infileName[MAXLEN];
 
-    for (int i = 0; found == 1; ++i) {
-        TMP_item = deleteByDataRange(data1,data2, p_list);
-        if(strcmp(TMP_item.nome, "NULL") == 0){
+    //CLIENT MENU'
 
-        } else{
-            itemPrint(TMP_item);
+    printf("Scegli un opzione: \n[1]Acquisizione da tastiera\n[2]Acquisizione da file\n[3]Ricerca per codice\n[4]Cancellazione per codice\n[5]Cancellazione in range di date\n[6]Stampa su file\n[0]Esci\n\n");
+    scanf("%d",&cmdIndex);
+
+    while(cmdIndex != 0){
+
+        switch (cmdIndex) {
+            case 1: //Acquisizione da tastiera
+                printf("Inserisci i valori:\ncodice nome cognome giorno mese anno via citta' cap\n\n");
+                scanf("%d %s %s %d %d %d %s %s %d",&TMP_item.codice,TMP_item.nome,TMP_item.cognome,&TMP_item.dataNascita.giorno,&TMP_item.dataNascita.mese,&TMP_item.dataNascita.anno,TMP_item.via,TMP_item.citta,&TMP_item.cap);
+                if ((p_list->head = listSortInsert(p_list,TMP_item)) == NULL) {
+                    printf("Impossibile allocare altro elemento in lista!\n\n");
+                }else{
+                    printf("Elemento inserito correttamente\n\n");
+                }
+                break;
+            case 2: //Acquisizione da file
+                printf("Inserisci nome del file:\n");
+                scanf("%s",infileName);
+                readReturn = fileRead(infileName,p_list);
+                if( readReturn == -1){
+                    printf("Impossibile allocare altro elemento in lista!\n\n");
+                } else if( readReturn == -2){
+                    printf("Impossibile aprire il file!\n\n");
+                }
+                else{
+                    printf("Elementi acquisiti da file correttamente!\n\n");
+                }
+
+                break;
+            case 3: //Ricerca per codice
+                printf("Inserisci codice da ricercare:\n");
+                scanf("%d",&TMP_code);
+                TMP_item = searchByCode(TMP_code,p_list);
+                if(strcmp(TMP_item.nome, "NULL")){
+                    itemPrint(TMP_item);
+                    printf("\n");
+                }else{
+                    printf("Nessun elemento trovato con questo codice!\n\n");
+                }
+                break;
+            case 4: //Cancellazione per codice
+                printf("Inserisci codice da eliminare:\n");
+                scanf("%d",&TMP_code);
+                TMP_item = deleteByCode(TMP_code,p_list);
+                if(strcmp(TMP_item.nome, "NULL")){
+                    itemPrint(TMP_item);
+                    printf("Elemento eliminato correttamente\n\n");
+                }else{
+                    printf("Nessun elemento trovato con questo codice!\n\n");
+                }
+                break;
+            case 5: //Cancellazione in range di date
+                printf("Inserisci data1 data2 (gg/mm/aaaa):\n");
+                scanf("%d/%d/%d %d/%d/%d", &data1.giorno,&data1.mese,&data1.anno,&data2.giorno,&data2.mese,&data2.anno);
+                for (int i = 0; found == 1; ++i) {
+                    TMP_item = deleteByDataRange(data1,data2, p_list);
+                    if(strcmp(TMP_item.nome, "NULL") == 0){
+
+                    } else{
+                        itemPrint(TMP_item);
+                    }
+                    printf("\n");
+                }
+                break;
+            case 6: //Stampa su file
+                printOnFile(OUTFILENAME, p_list);
+                break;
+            default:
+                printf("Digita un'opzione valida!\n\n");
+                break;
+
+
         }
+
+        printf("Scegli un opzione: \n[1]Acquisizione da tastiera\n[2]Acquisizione da file\n[3]Ricerca per codice\n[4]Cancellazione per codice\n[5]Cancellazione in range di date\n[6]Stampa su file\n[0]Esci\n\n");
+        scanf("%d",&cmdIndex);
     }
 
-    printOnFile("../output.txt", p_list);*/
+    //-----------
 
     return 0;
 }
@@ -128,22 +191,22 @@ s_node *listSortInsert(s_list *p_list, s_item item){
 
 }
 
-void fileRead(char *fileName, s_list *p_list){
+int fileRead(char *fileName, s_list *p_list){
 
     FILE *fIn;
     s_item TMP_item;
     s_node *TMPp_head;
 
-    fIn = fopen(fileName,"r");
+    if((fIn = fopen(fileName,"r")) == NULL) return -2;
     while ((fscanf(fIn, "A%d %s %s %d/%d/%d %s %s %d ",&TMP_item.codice,TMP_item.nome,TMP_item.cognome,&TMP_item.dataNascita.giorno,&TMP_item.dataNascita.mese,&TMP_item.dataNascita.anno,TMP_item.via,TMP_item.citta,&TMP_item.cap)) != EOF){
         TMPp_head = listSortInsert(p_list, TMP_item);
         if(TMPp_head == NULL){
-            printf("Impossibile allocare memoria!");
-            return;
+            return -1;
         }
         p_list->head = TMPp_head;
     }
     fclose(fIn);
+    return 0;
 
 }
 
@@ -179,26 +242,30 @@ void itemPrint(s_item item){
 
 }
 
-s_node *deleteByCode(int code, s_list *p_list){
+s_item deleteByCode(int code, s_list *p_list){
 
     s_node *x, *p, *tmp;
-
+    s_item TMP_item;
+    if(p_list->head == NULL) return itemVoid();
     if(p_list->head->item.codice == code){
+        TMP_item = p_list->head->item;
         tmp = p_list->head->next;
         free(p_list->head);
         (p_list->N)--;
-        return tmp;
+        p_list->head = tmp;
+        return TMP_item;
     }
 
     p = NULL;
     for (x = p_list->head; x != NULL && x->item.codice != code; p = x, x = x->next){
     }
-    if(x == NULL) return NULL;
+    if(x == NULL) return itemVoid();
+    TMP_item = x->item;
     tmp = x;
     p->next = x->next;
     (p_list->N)--;
     free(tmp);
-    return p_list->head;
+    return TMP_item;
 
 }
 
@@ -208,6 +275,7 @@ s_item deleteByDataRange(s_data data1, s_data data2, s_list *p_list){
     s_item TMP_item;
 
     found = 0;
+    if(p_list->head == NULL) return itemVoid();
     if(dataGrater(p_list->head->item.dataNascita, data1) && dataGrater(data2, p_list->head->item.dataNascita)){
         found = 1;
         TMP_item = p_list->head->item;
