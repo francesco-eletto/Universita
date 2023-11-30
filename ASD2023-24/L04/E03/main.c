486834b9bae619b6a13abc24fb5bb091d5708343
Francesco <francesco.eletto02@gmail.com>
2023-11-30 11:47:08 +0100

    ASD23-24/L04/E03/main.c sequenceMaker function added without check and pruning. collanaCheckNum and collanaUpdateNum functions added

--- ASD2023-24/L04/E03/main.c
+++ ASD2023-24/L04/E03/main.c
@@ -2,6 +2,7 @@
 #include "string.h"
 #include "malloc.h"
 #define LEN 4
+#define MAXLEN 100
 
 typedef struct {
     int nz, nr, nt, ns;
@@ -10,7 +11,10 @@ typedef struct {
 int found;
 
 void sequenceMaker(s_collana collana);
-void r_disp_ripet(int pos, char *val, char *sol, int n, int k);
+void r_disp_ripet(int pos, char *val, char *sol, int n, int k, s_collana *collana);
+int collanaCheckNum(s_collana collana, int index);
+void collanaUpdateNum(s_collana *collana, int index, int value);
+
 
 
 int main(int argc, char **argv) {
@@ -33,22 +37,20 @@ int main(int argc, char **argv) {
 void sequenceMaker(s_collana collana){
 
     int len;
-    char *sol, val[LEN];
+    char sol[MAXLEN], val[LEN];
 
     found = 0;
     len = collana.ns + collana.nt + collana.nz + collana.nr;
-    sol = (char *)malloc(len*sizeof(char));
     strcpy(val,"zrts");
 
     for (int i = len; i > 0 && found == 0; --i) {
-        r_disp_ripet(0,val,sol,LEN,i);
+        r_disp_ripet(0,val,sol,LEN,i, &collana);
     }
 
-    free(sol);
 
 }
 
-void r_disp_ripet(int pos, char *val, char *sol, int n, int k){
+void r_disp_ripet(int pos, char *val, char *sol, int n, int k, s_collana *collana){
     if(found) return;
     if(pos >= k){
         found = 1;
@@ -60,8 +62,48 @@ void r_disp_ripet(int pos, char *val, char *sol, int n, int k){
     }
 
     for (int i = 0; i < n; ++i) {
-        sol[pos] = val[i];
-        r_disp_ripet(pos+1,val,sol,n,k);
+        if(collanaCheckNum(*collana,i)){
+            collanaUpdateNum(collana,i,-1);
+            sol[pos] = val[i];
+            r_disp_ripet(pos+1,val,sol,n,k,collana);
+            collanaUpdateNum(collana,i,1);
+        }
     }
     return;
+}
+
+int collanaCheckNum(s_collana collana, int index){
+
+    switch (index) {
+        case 0:
+            if(collana.nz > 0) return 1;
+            break;
+        case 1:
+            if(collana.nr > 0) return 1;
+            break;
+        case 2:
+            if(collana.nt > 0) return 1;
+            break;
+        case 3:
+            if(collana.ns > 0) return 1;
+            break;
+    }
+    return 0;
+}
+
+void collanaUpdateNum(s_collana *collana, int index, int value){
+    switch (index) {
+        case 0:
+            collana->nz = collana->nz + value;
+            break;
+        case 1:
+            collana->nr = collana->nr + value;
+            break;
+        case 2:
+            collana->nt = collana->nt + value;
+            break;
+        case 3:
+            collana->ns = collana->ns + value;
+            break;
+    }
 }
