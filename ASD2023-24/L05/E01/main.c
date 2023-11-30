5ce7b4ccde5b6f6ab91d085e35048c4179113d13
Francesco <francesco.eletto02@gmail.com>
2023-11-30 17:27:00 +0100

    ASD2023-24/L04/E01/main.c powerset_disp_rip_ricors and sequenceMaker function added

--- ASD2023-24/L05/E01/main.c
+++ ASD2023-24/L05/E01/main.c
@@ -6,6 +6,9 @@ typedef struct{
 }s_att;
 
 s_att *fileRead(char *fileName, int *p_len);
+void sequenceMaker(int len, s_att *att);
+void powerset_disp_rip_ricors(int pos, int *sol, int *val, int n, s_att *att);
+
 
 int main(int argc, char **argv) {
 
@@ -13,7 +16,7 @@ int main(int argc, char **argv) {
     int len;
 
     p_att = fileRead(argv[1], &len);
-
+    sequenceMaker(len,p_att);
     return 0;
 }
 
@@ -24,7 +27,7 @@ s_att *fileRead(char *fileName, int *p_len){
 
     fIn = fopen(fileName,"r");
     fscanf(fIn,"%d",p_len);
-    TMPp_att = (s_att *)malloc(*p_len * sizeof(s_att));
+    TMPp_att = (s_att *)malloc(*p_len * sizeof(s_att)); //TODO deallocare
 
     for (int i = 0; i < *p_len; ++i) {
         fscanf(fIn, "%d %d",&TMPp_att[i].sTime, &TMPp_att[i].fTime);
@@ -32,4 +35,31 @@ s_att *fileRead(char *fileName, int *p_len){
     }
 
     return TMPp_att;
+}
+
+void sequenceMaker(int len, s_att *p_att){
+
+    int *sol, *val; //TODO deallocare
+
+    sol = (int *)malloc(len * sizeof(int));
+    val = (int *)malloc(len * sizeof(int));
+    powerset_disp_rip_ricors(0,sol,val,len,p_att);
+
+}
+
+void powerset_disp_rip_ricors(int pos, int *sol, int *val, int n, s_att *p_att){
+
+    if(pos >= n){
+        for (int i = 0; i < n; ++i) {
+            if(sol[i] == 1) printf("%d %d, ", p_att[i].sTime, p_att[i].fTime);
+        }
+        printf("\n");
+        return;
+    }
+
+
+    sol[pos] = 1;
+    powerset_disp_rip_ricors(pos+1,sol,val,n,p_att);
+    sol[pos] = 0;
+    powerset_disp_rip_ricors(pos+1,sol,val,n,p_att);
 }
