eebcf7f20b3620a0b4bdfea9cefabb9b967c4965
Francesco <francesco.eletto02@gmail.com>
2023-12-05 18:48:29 +0100

    ASD23-24/L06/E02/main.c file reading implemented

--- ASD2023-24/L06/E02/main.c
+++ ASD2023-24/L06/E02/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <malloc.h>
 
-int main() {
+typedef struct {
+
+    int nz,nr,nt,ns;
+}s_collana;
+
+int main(int argc, char **argv) {
+
+    FILE *fIn;
+    s_collana collana;
+    int len;
+
+    fIn = fopen(argv[1], "r"); //TODO chiudere
+    fscanf(fIn,"%d",&len);
+
+    for (int i = 0; i < len; ++i) {
+        fscanf(fIn, " %d %d %d %d", &collana.nz, &collana.nr, &collana.nt, &collana.ns);
+    }
 
     return 0;
 }
