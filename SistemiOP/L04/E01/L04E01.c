85608812d3c49f3ee673791d0e9705ff9ffd127f
francesco-eletto <francesco.eletto02@gmail.com>
2024-01-06 19:21:30 +0100

    L04E01.c

--- SistemiOP/L04/E01/L04E01.c
+++ SistemiOP/L04/E01/L04E01.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-int main{
+int main(){
 
 int val;
-
+do {
 fscanf(stdin,"%d",&val);
 if(val%2){
     fprintf(stderr,"%d",val);
 }else{
     fprintf(stdout,"%d",val);
 }
+} while(val!=0);
 return 0;
 }
